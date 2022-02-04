74d4c620ce7900a4385f20b0cac08966285b5ea5
guguldash <84378775+guguldash@users.noreply.github.com>
2022-02-04 11:38:17 +0530

    Add files via upload

---
+++ strong number.py
@@ -0,0 +1,17 @@
+number=int(input('enter your number'))
+sum=0
+rem=0
+t=number
+while number>0:
+	factor=1
+	i=1
+	reminder=number%10
+	while i<=reminder:
+		factor=factor*i
+		i=i+1
+	sum=sum+factor
+	number=number//10
+if sum==t:
+	print(t,'strong number')
+else:
+	print(t,'not a strong number')
